35796043014b112ede8320ba6ee8c117259f13b6
Riuji <riuji0x@gmail.com>
2023-03-12 13:45:31 +0200

    Added Server > Client - notification func

--- basic_funcs.lua
+++ basic_funcs.lua
@@ -1,16 +1,22 @@
-local function split (inputstr, sep)
-    if sep == nil then
-        sep = "%s"
-    end
-    local t={}
-    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-        table.insert(t, str)
-    end
-    return t
+function split (inputstr, sep)
+   if sep == nil then
+      sep = "%s"
+   end
+   local t={}
+   for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+      table.insert(t, str)
+   end
+   return t
 end
 
-local function steamIDQuery( steamid )
+function steamIDQuery( steamid )
     for ind, ply in ipairs(find.allPlayers()) do
         if ply:getSteamID() == steamid then return ply end
     end
+end
+
+function serverClientNotification( _text, _type, _time )
+    net.start("CLIENT_NOTIFICATION")
+        net.writeTable({_text, _type, _time})
+    net.send(owner())
 end
